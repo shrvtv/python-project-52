896af695cde0e84cc7f34bf9d743dfe7617fd80e
Artem Sharovatov <git@sharovatov.me>
2026-02-22 20:32:56 +0300

    Add login page

--- task_manager/users/forms.py
+++ task_manager/users/forms.py
@@ -1,5 +1,6 @@
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
+from django.utils.translation import gettext
 
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
@@ -10,4 +11,14 @@ class CustomUserCreationForm(UserCreationForm):
         super().__init__(*args, **kwargs)
         for field in self.fields.values():
             field.widget.attrs['class'] = 'form-control'
-            field.widget.attrs['placeholder'] = field.label
+            field.widget.attrs['required'] = ''
+            field.widget.attrs['placeholder'] = gettext(field.label)
+
+
+class CustomAuthenticationForm(AuthenticationForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs['class'] = 'form-control'
+            field.widget.attrs['required'] = ''
+            field.widget.attrs['placeholder'] = gettext(field.label)
--- task_manager/views.py
+++ task_manager/views.py
@@ -3,13 +3,16 @@ from django.urls import reverse_lazy
 import django.contrib.auth.views as auth_views
 import django.views.generic as generic_views
 
+from task_manager.users.forms import CustomAuthenticationForm
 
 class IndexView(generic_views.TemplateView):
     template_name = "index.html"
 
 
 class LoginView(auth_views.LoginView):
+    form_class = CustomAuthenticationForm
     next_page = reverse_lazy("index")
+    template_name = "login.html"
 
 
 class LogoutView(auth_views.LogoutView):
