f87ea0cd32ae717cd998353797bfdf59cff7fbc2
Artem Sharovatov <git@sharovatov.me>
2026-02-22 16:42:15 +0300

    Add registration support

---
+++ task_manager/users/forms.py
@@ -0,0 +1,13 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'username', 'password1', 'password2')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs['class'] = 'form-control'
+            field.widget.attrs['placeholder'] = field.label
--- task_manager/users/views.py
+++ task_manager/users/views.py
@@ -1,5 +1,8 @@
 import django.views.generic as generic_views
 from django.contrib.auth.models import User
+from django.urls import reverse_lazy
+
+from task_manager.users.forms import CustomUserCreationForm
 
 
 class UserListView(generic_views.ListView):
@@ -9,7 +12,9 @@ class UserListView(generic_views.ListView):
 
 
 class UserCreateView(generic_views.CreateView):
-    pass
+    form_class = CustomUserCreationForm
+    success_url = reverse_lazy("users:list")
+    template_name = "task_manager/users/create.html"
 
 
 class UserUpdateView(generic_views.UpdateView):
