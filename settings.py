8009d0b843298ab06d9aded0d665b347b15eb858
Artem Sharovatov <git@sharovatov.me>
2026-02-23 16:42:49 +0300

    Add whitenoise for static file serving

--- settings.py
+++ settings.py
@@ -42,6 +42,12 @@ if render_host:
 
 AUTH_USER_MODEL = 'auth.User'
 
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -56,6 +62,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
