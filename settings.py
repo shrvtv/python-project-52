973cdd244e684ed4d5e7f90da9e80cbd2923b5b5
Artem Sharovatov <git@sharovatov.me>
2026-02-24 17:32:34 +0300

    Add production DB connection

--- settings.py
+++ settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 from pathlib import Path
 from dotenv import load_dotenv
 import os
+import dj_database_url
 from django.utils.translation import gettext_lazy
 
 BASE_DIR = Path(__file__).resolve().parent
@@ -96,10 +97,10 @@ WSGI_APPLICATION = 'task_manager.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
 }
 
 
