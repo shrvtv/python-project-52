80ff9ec4d5fdfab82ad2fced3879b6a134e6a3bc
Artem Sharovatov <git@sharovatov.me>
2026-02-25 13:52:06 +0300

    Add new views in statuses app

--- task_manager/statuses/urls.py
+++ task_manager/statuses/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path("", status_views.StatusListView.as_view(), name="list"),
     path("create/", status_views.StatusCreateView.as_view(), name="create"),
     path("<int:pk>/update/", status_views.StatusUpdateView.as_view(), name="update"),
-    path("<int:pk>/delete/", status_views.StatusDeleteView.as_view(), name="delete")
+    path("<int:pk>/delete/", status_views.StatusDeleteView.as_view(), name="delete"),
 ]
--- task_manager/statuses/views.py
+++ task_manager/statuses/views.py
@@ -8,19 +8,18 @@ from django.utils.translation import gettext_lazy
 app_label = "statuses"
 
 
-class StatusBaseView(mixins.LoginRequiredMixin):
+class StatusMixin(mixins.LoginRequiredMixin):
     model = Status
-    form_class = StatusCreationForm
     login_url = reverse_lazy("login")
+    def get_success_url(self):
+        return reverse_lazy("statuses:list")
 
 
 class StatusCreateView(
-    StatusBaseView,
-    views.CreateView
+    StatusMixin,
+    views.CreateView,
 ):
     form_class = StatusCreationForm
-    login_url = reverse_lazy("login")
-    success_url = reverse_lazy("statuses:list")
     template_name = "task_manager/statuses/form.html"
     extra_context = {
         "header": gettext_lazy("Create status"),
@@ -29,26 +28,24 @@ class StatusCreateView(
 
 
 class StatusDeleteView(
-    StatusBaseView,
-    views.DeleteView
-    
+    StatusMixin,
+    views.DeleteView,
 ):
-    pass
+    template_name = "task_manager/statuses/delete.html"
 
 
 class StatusListView(
-    StatusBaseView,
-    views.ListView
+    StatusMixin,
+    views.ListView,
 ):
-    queryset = Status.objects.all()
     template_name = "task_manager/statuses/list.html"
 
 
 class StatusUpdateView(
-    StatusBaseView,
-    views.UpdateView
+    StatusMixin,
+    views.UpdateView,
 ):
-    success_url = reverse_lazy("statuses:list")
+    form_class = StatusCreationForm
     template_name = "task_manager/statuses/form.html"
     extra_context = {
         "header": gettext_lazy("Edit status"),
