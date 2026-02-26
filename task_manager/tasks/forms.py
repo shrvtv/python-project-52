591786a11f92710406b8e888c0e801c424777689
Artem Sharovatov <git@sharovatov.me>
2026-02-26 23:12:22 +0300

    Add TaskFilterForm

--- task_manager/tasks/forms.py
+++ task_manager/tasks/forms.py
@@ -1,6 +1,22 @@
 from django import forms
 from task_manager.tasks.models import Task
 from django.utils.translation import gettext
+from task_manager.statuses.models import Status
+from django.contrib.auth.models import User
+
+
+class TaskFilterForm(forms.Form):
+    status = forms.ModelChoiceField(
+        queryset=Status.objects.all(),
+        required=False,
+        empty_label="---------"
+    )
+    executor = forms.ModelChoiceField(
+        queryset=User.objects.all(),
+        required=False,
+        empty_label="---------"
+    )
+    self_tasks = forms.BooleanField(required=False)
 
 
 class TaskCreationForm(forms.ModelForm):
