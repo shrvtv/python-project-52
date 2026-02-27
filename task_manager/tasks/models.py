b44458e204a105ed9dd27517c505e1ab85ed4f1c
Artem Sharovatov <git@sharovatov.me>
2026-02-27 14:08:41 +0300

    Add labels app and Label model

--- task_manager/tasks/models.py
+++ task_manager/tasks/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 from task_manager.statuses.models import Status
+from task_manager.labels.models import Label
+
 
 class Task(models.Model):
     name = models.CharField(max_length=255)
@@ -22,4 +24,9 @@ class Task(models.Model):
         blank=True,
         null=True,
         related_name="tasks_executing"
+    )
+    labels = models.ManyToManyField(
+        Label,
+        blank=True,
+        related_name="tasks_assigned"
     )
