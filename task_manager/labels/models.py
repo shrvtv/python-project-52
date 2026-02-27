865cb6e0f53ff45fbdf9625b8b24c998ff5e6262
Artem Sharovatov <git@sharovatov.me>
2026-02-27 15:24:51 +0300

    Add name uniqueness requirment to Label model

--- task_manager/labels/models.py
+++ task_manager/labels/models.py
@@ -2,5 +2,5 @@ from django.db import models
 
 
 class Label(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
