6d1bd963882308fae52c88891ace030428bb96b2
Artem Sharovatov <git@sharovatov.me>
2026-03-02 13:28:17 +0300

    Restrict users from deleting a label with tasks assigned

--- task_manager/tasks/models.py
+++ task_manager/tasks/models.py
@@ -4,29 +4,35 @@ from task_manager.statuses.models import Status
 from task_manager.labels.models import Label
 
 
+class TaskLabel(models.Model):
+    task = models.ForeignKey("Task", on_delete=models.CASCADE)
+    label = models.ForeignKey(Label, on_delete=models.RESTRICT)
+
+
 class Task(models.Model):
     name = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
     status = models.ForeignKey(
         Status,
         on_delete=models.RESTRICT,
-        related_name="tasks_assigned"
+        related_name="tasks_assigned",
     )
     description = models.TextField(blank=True, null=True)
     author = models.ForeignKey(
         User,
         on_delete=models.RESTRICT,
-        related_name="tasks_authored"
+        related_name="tasks_authored",
     )
     executor = models.ForeignKey(
         User,
         on_delete=models.SET_NULL,
         blank=True,
         null=True,
-        related_name="tasks_executing"
+        related_name="tasks_executing",
     )
     labels = models.ManyToManyField(
         Label,
+        through=TaskLabel,
         blank=True,
-        related_name="tasks_assigned"
+        related_name="tasks_assigned",
     )
