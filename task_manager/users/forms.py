e666ca3af1c5f25c4e296361434843f649ef77a6
Artem Sharovatov <git@sharovatov.me>
2026-03-07 15:55:24 +0300

    Add translation to user authentication and creation forms

--- task_manager/users/forms.py
+++ task_manager/users/forms.py
@@ -1,24 +1,33 @@
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
-from django.utils.translation import gettext
+from django.utils.translation import gettext_lazy
+from django import forms
 
 class CustomUserCreationForm(UserCreationForm):
+    first_name = forms.CharField(label=gettext_lazy("First name"))
+    last_name = forms.CharField(label=gettext_lazy("Last name"))
+
     class Meta:
         model = User
         fields = ('first_name', 'last_name', 'username', 'password1', 'password2')
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.fields["username"].label = gettext_lazy("Username")
+        self.fields["password1"].label = gettext_lazy("Password")
+        self.fields["password2"].label = gettext_lazy("Password confirmation")
         for field in self.fields.values():
-            field.widget.attrs['class'] = 'form-control'
-            field.widget.attrs['required'] = ''
-            field.widget.attrs['placeholder'] = gettext(field.label)
+            field.widget.attrs["class"] = "form-control"
+            field.widget.attrs["required"] = ""
+            field.widget.attrs["placeholder"] = field.label
 
 
 class CustomAuthenticationForm(AuthenticationForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.fields["username"].label = gettext_lazy("Username")
+        self.fields["password"].label = gettext_lazy("Password")
         for field in self.fields.values():
-            field.widget.attrs['class'] = 'form-control'
-            field.widget.attrs['required'] = ''
-            field.widget.attrs['placeholder'] = gettext(field.label)
+            field.widget.attrs["class"] = "form-control"
+            field.widget.attrs["required"] = ""
+            field.widget.attrs["placeholder"] = field.label
