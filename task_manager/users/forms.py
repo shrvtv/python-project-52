994b7c1546cac01cfcbed1be14156252cdb78374
Artem Sharovatov <git@sharovatov.me>
2026-03-17 16:24:23 +0300

    Fix user update form

--- task_manager/users/forms.py
+++ task_manager/users/forms.py
@@ -1,9 +1,19 @@
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
-from django.utils.translation import gettext_lazy
+from django.utils.translation import gettext_lazy, gettext
 from django import forms
 
-class CustomUserCreationForm(UserCreationForm):
+
+class StyleMixin:
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs["class"] = "form-control"
+            field.widget.attrs["required"] = ""
+            field.widget.attrs["placeholder"] = field.label
+
+
+class CustomUserCreationForm(StyleMixin, UserCreationForm):
     first_name = forms.CharField(label=gettext_lazy("First name"))
     last_name = forms.CharField(label=gettext_lazy("Last name"))
 
@@ -17,12 +27,16 @@ class CustomUserCreationForm(UserCreationForm):
             'password2',
         )
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        for field in self.fields.values():
-            field.widget.attrs["class"] = "form-control"
-            field.widget.attrs["required"] = ""
-            field.widget.attrs["placeholder"] = field.label
+
+class CustomUserUpdateForm(StyleMixin, CustomUserCreationForm):
+    def clean_username(self):
+        username = self.cleaned_data["username"]
+        qs = User.objects.exclude(pk=self.instance.pk)
+        if qs.filter(username=username).exists():
+            raise forms.ValidationError(
+                gettext("A user with that username already exists.")
+            )
+        return username
 
 
 class CustomAuthenticationForm(AuthenticationForm):
