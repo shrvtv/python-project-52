ac70771bbe7540b26dd5569e76b2d6348f547863
Artem Sharovatov <git@sharovatov.me>
2026-03-19 13:39:20 +0300

    Add login error message

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,11 +1,12 @@
 from django.urls import reverse_lazy
 from django.contrib import messages
-from django.utils.translation import gettext_lazy
+from django.utils.translation import gettext
 
 import django.contrib.auth.views as auth_views
 import django.views.generic as generic_views
 
 from task_manager.users.forms import CustomAuthenticationForm
+
 
 class IndexView(generic_views.TemplateView):
     template_name = "index.html"
@@ -18,9 +19,16 @@ class LoginView(auth_views.LoginView):
     def form_valid(self, form):
         response = super().form_valid(form)
         messages.success(
-            self.request, gettext_lazy("You are logged in")
+            self.request, gettext("You are logged in")
         )
         return response
+
+    def form_invalid(self, form):
+        messages.error(
+            self.request,
+            gettext("Please enter a correct username and password")
+        )
+        return super().form_invalid(form)
 
 
 class LogoutView(auth_views.LogoutView):
@@ -28,5 +36,5 @@ class LogoutView(auth_views.LogoutView):
     next_page = reverse_lazy("users:list")
     def post(self, request, *args, **kwargs):
         response = super().post(request, *args, **kwargs)
-        messages.success(request, gettext_lazy("You logged out"))
+        messages.success(request, gettext("You logged out"))
         return response
